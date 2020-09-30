6dca6137c4231992d2739485e215d5f96df80e81
zzq <2455676683@qq.com>
2020-09-30 17:43:27 +0800

    yoyiyi

--- app/src/main/java/com/yoyiyi/algorithm/Ask006.kt
+++ app/src/main/java/com/yoyiyi/algorithm/Ask006.kt
@@ -8,17 +8,17 @@ package com.yoyiyi.algorithm
  *
  */
 
-class ListNode{
-    var any :Int?=null
-    var next :ListNode?=null
+class ListNode {
+    var any: Int? = null
+    var next: ListNode? = null
 }
 
-class Ask006{
+class Ask006 {
 
-    fun printListFromTailToHead1(listNode:ListNode):MutableList<Int>{
+    fun printListFromTailToHead1(listNode: ListNode): MutableList<Int> {
         val list = mutableListOf<Int>()
-        var tmp:ListNode?  = listNode
-        while(tmp!=null) {
+        var tmp: ListNode? = listNode
+        while (tmp != null) {
             list.add(0, tmp.any!!)
             tmp = tmp.next
         }
@@ -28,8 +28,9 @@ class Ask006{
 
     //递归
     val list = mutableListOf<Int>()
-    fun printListFromTailToHead2(listNode:ListNode?):MutableList<Int>{
-        if(listNode!=null){
+
+    fun printListFromTailToHead2(listNode: ListNode?): MutableList<Int> {
+        if (listNode != null) {
             printListFromTailToHead2(listNode.next)
             list.add(listNode.any!!)
         }
